cd7ecc6f6583d30f2aff6cdb4948add7fece859a
funhappyit <fun.happyit@gmail.com>
2021-11-01 17:04:36 +0900

    2021-11-01 1) 스프링 시큐리티 회원가입

--- src/main/java/org/cos/security1/controller/IndexController.java
+++ src/main/java/org/cos/security1/controller/IndexController.java
@@ -1,11 +1,22 @@
 package org.cos.security1.controller;
 
+import org.cos.security1.model.User;
+import org.cos.security1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@GetMapping({"","/"})
 	public String index() {
@@ -30,19 +41,27 @@ public class IndexController {
 	}
 	
 	//스프링 시큐리티가 해당 주소를 낚아채버림 -> SecurityConfig 파일 생성 후 작동안함
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	//회원가입 페이지 이동
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
 	
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "회원가입 완료됨!";
+	//실제 회원가입 
+	@PostMapping("/join")
+	public String join(User user) {
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		
+		userRepository.save(user); //회원가입 잘됨. 비밀번호:1234=> 시큐리티로 로그인 할 수 없음 암호화가 안되어있기 때문에  
+		return "redirect:/loginForm";
 	}
 
 }
